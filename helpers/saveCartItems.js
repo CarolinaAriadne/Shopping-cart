afd75ef514f9f839c1ecd55bf29bee5db1afa3bd
Carolina Ariadne <c.ariadneadv@gmail.com>
2021-12-04 14:04:51 -0300

    Requisito-3

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,7 +1,6 @@
-const saveCartItems = () => {
-  // seu código aqui
-};
-
+const saveCartItems = (item) => localStorage.setItem('cartItems', item);
 if (typeof module !== 'undefined') {
   module.exports = saveCartItems;
 }
+
+// obs: localStorage.setItem() - quando passo chave e valor, essa chave será adicionada ao storage, ou atualizar o valor, se a chave já existir.
